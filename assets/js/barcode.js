aeda5b45a05bd155ba1553a4e6c61798ffacf720
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-30 01:17:40 +0200

    Update barcode.js

--- assets/js/barcode.js
+++ assets/js/barcode.js
@@ -1,11 +1,8 @@
-let buffer = "";
-
-document.addEventListener("keydown", e => {
-  if (e.key === "Enter") {
-    searchProduct(buffer);
-    new Audio("assets/sounds/beep.mp3").play();
-    buffer = "";
-  } else {
-    buffer += e.key;
+const beep = new Audio("../assets/sounds/beep.mp3");
+function barcodeScan(code){
+  const p = products.find(x=>x.barcode==code);
+  if(p){
+    beep.play();
+    addToInvoice(p);
   }
-});
+}
