61e660698592f84344f69768a0eb4c3e5fcd162e
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-30 01:14:12 +0200

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,28 +1,7 @@
-function login() {
+function login(){
   const u = username.value;
   const p = password.value;
-
-  const users = JSON.parse(localStorage.users || "[]");
-  const user = users.find(x => x.u === u && x.p === p);
-
-  if (!user) {
-    msg.innerText = "بيانات غير صحيحة";
-    return;
-  }
-
-  localStorage.session = JSON.stringify(user);
-  location.href = "index.html";
-}
-
-function register() {
-  const u = username.value;
-  const p = password.value;
-
-  if (!u || !p) return;
-
-  const users = JSON.parse(localStorage.users || "[]");
-  users.push({ u, p });
-  localStorage.users = JSON.stringify(users);
-
-  msg.innerText = "تم إنشاء الحساب – سجل دخول";
+  if(!u||!p) return alert("ادخل البيانات");
+  localStorage.user = u;
+  location.href="index.html";
 }
