4cf9bfc8fa5276d92402ef084fe9f51da78380f4
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-30 23:33:40 +0200

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -10,3 +10,14 @@ window.onload = () => {
     document.body.classList.add("dark");
   }
 };
+function toggleTheme(){
+  document.body.classList.toggle("dark");
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("dark") ? "dark" : "light"
+  );
+}
+
+if(localStorage.getItem("theme")==="dark"){
+  document.body.classList.add("dark");
+}
