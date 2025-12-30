c52756cceb8364680e1ae987bf6543e7d6372f3d
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-30 13:33:14 +0200

    Update settings.js

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -1,15 +1,3 @@
-function setTheme(t){
-  document.body.className = t;
-  localStorage.theme = t;
-}
-
-function resetData(){
-  if(confirm("متأكد؟")){
-    localStorage.clear();
-    location.href = "../login.html";
-  }
-}
-
-if(localStorage.theme){
-  document.body.className = localStorage.theme;
+function toggleTheme(){
+  document.body.classList.toggle("dark");
 }
