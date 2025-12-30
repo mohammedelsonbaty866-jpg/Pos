8d24bdd953af519cf1762fefd416e172301c2101
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-31 01:37:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,46 +1,50 @@
 const CACHE_NAME = "pos-super-pro-v1";
 
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/login.html",
-  "/manifest.json",
-
-  "/assets/css/style.css",
-  "/assets/css/auth.css",
-  "/assets/css/theme.css",
-
-  "/assets/js/auth.js",
-  "/assets/js/auth-guard.js",
-  "/assets/js/data.js",
-  "/assets/js/products.js",
-  "/assets/js/cashier.js",
-  "/assets/js/barcode.js",
-  "/assets/js/settings.js",
-
-  "/assets/sounds/beep.mp3"
+  "./",
+  "./index.html",
+  "./login.html",
+  "./manifest.json",
+  "./assets/css/style.css",
+  "./assets/css/auth.css",
+  "./assets/css/theme.css",
+  "./assets/js/auth.js",
+  "./assets/js/auth-guard.js",
+  "./assets/js/data.js",
+  "./assets/js/products.js",
+  "./assets/js/cashier.js",
+  "./assets/js/reports.js",
+  "./assets/js/settings.js",
+  "./assets/js/barcode.js",
+  "./assets/sounds/beep.mp3"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
-  e.waitUntil(
+self.addEventListener("activate", event => {
+  event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.map(k => k !== CACHE_NAME && caches.delete(k))
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
